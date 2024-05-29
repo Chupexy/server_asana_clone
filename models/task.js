ee53e161e407258f61b4ed93e9de03e2e3f077fe
chupexy <osaretin520@gmail.com>
2024-05-29 08:16:57 +0100

    new endpoints

--- models/task.js
+++ models/task.js
@@ -5,12 +5,11 @@ const taskSchema = new Schema({
 user_id: String,
 task_name: String,
 due_date: String, // ill confirm if its a string or number later
-asignee: String,
 assigned_to: [String],
 project: String,
 description: String,
-privacy: {type: String, default: 'private'}, // public, if you want other persons to access it
-completion_status: {type: String, default: 'Not started'}, // not started, in progress, completed, on hold, cancelled, testing
+privacy_status: {type: String, default: "private"}, // public, if you want other persons to access it
+completion_status: {type: String, default: "Not started"}, // not started, in progress, completed, on hold, cancelled, testing
 is_overdue: {type: Boolean, default: false},
 timestamp: Number,
 priority: {type: String, default: ''}, // high, medium, low, only for tasks under a project
--- route_user/tasks.js
+++ route_user/tasks.js
@@ -3,11 +3,212 @@ const router = express.Router()
 const jwt = require('jsonwebtoken')
 const dotenv = require('dotenv')
 dotenv.config()
-const task = require('../models/task')
-const project = require('../models/project')
+const Task = require('../models/task')
+const Project = require('../models/project')
 const comment = require('../models/comments')
 
 
 router.post('/create_task', async(req, res) =>{
-    
+    const {token , task_name} = req.body
+    if(!token || !task_name)
+        return res.status(400).send({status: 'error', msg:'All fields must be filled'})
+
+    try {
+        //verify token
+        const user = jwt.verify(token, process.env.JWT_SECRET)
+        const timestamp = Date.now()
+
+        const task = new Task()
+        task.user_id = user._id
+        task.task_name = task_name
+        task.due_date= ""
+        task.assigned_to = []
+        task.project = ""
+        task.description = ""
+        task.timestamp = timestamp
+        task.comments = []
+
+        await task.save()
+
+        return res.status(200).send({status: 'ok', msg:'Task created successful', task})
+
+
+    } catch (e) {
+        console.log(e)
+        if(e.name === 'JsonWebTokenError')
+            return res.status(401).send({status: 'error', msg:'Token verification failed'})
+
+        return res.status(500).send({status:'error', msg:'An error occured'})
+        
+    }
 })
+
+//endpoint to edit task
+router.post('/edit_task', async(req, res) =>{
+       const {token,task_name, due_date, description, task_id} = req.body
+    if(!token || !task_id)
+        return res.status(400).send({status: 'error', msg:'All fields must be filled'})
+
+    try {
+        //verify token
+        const user =jwt.verify(token, process.env.JWT_SECRET)
+
+        //get task document
+        const task = await Task.findOne({task_id, user_id: user._id}, {task_name: 1, due_date: 1, description: 1}).lean()
+        if(!task)
+            return res.status(200).send({status: 'ok', msg:'Task not found'})
+
+        task = await Task.findByIdAndUpdate({task_id}, {
+            task_name: task_name || task.task_name,
+            due_date: due_date || task.due_date,
+            description: description || task.description
+        }, {new: true}).lean()
+
+        return res.status(200).send({status: 'ok', msg:'Task updated successfully', task})
+    } catch (e) {
+        console.log(e)
+        if(e.name === 'JsonWebTokenError')
+            return res.status(401).send({status: 'error', msg:'Token verification failed'})
+
+        return res.status(500).send({status:'error', msg:'An error occured'})
+    }
+})
+
+//endpoint to view single task
+router.post('/view_task', async(req, res) =>{
+    const { token, task_id } = req.body;
+
+    if(!token || !task_id)
+        return res.status(400).send({status: 'error', msg:'All fields must be filled'});
+
+    try {
+        //verify token
+         jwt.verify(token, process.env.JWT_SECRET)
+
+        //find product documeent
+        const task = await Task.findOne({_id: task_id}).lean()
+
+        if(!task)
+            return res.status(404).send({status: 'error', msg: 'Task not found'})
+
+        return res.status(200).send({status:'ok', msg:'Task found', task})
+        
+    } catch (e) {
+        console.log(e)
+        if(e.name === 'JsonWebTokenError'){
+            return res.status(401).send({status:'error', msg:'Token verification failed', error: e})
+            }
+            return res.status(500).send({status:'error', msg:'An error occured'})
+        
+    }
+})
+
+// endpoint to view tasks
+router.post('/view_tasks', async(req, res) =>{
+    const { token } = req.body;
+
+    if(!token)
+        return res.status(400).send({status: 'error', msg:'All fields must be filled'});
+
+    try {
+        jwt.verify(token, process.env.JWT_SECRET)
+
+        const tasks = await Task.find({ }).lean()
+        if(tasks.length == 0)
+            return res.status(400).send({status:'error', msg:'No task at the moment'});
+
+        return res.status(200).send({status:'ok', msg:'Tasks found', tasks, count: tasks.length})
+    } catch (e) {
+        console.log(e)
+        if(e.name === 'JsonWebTokenError'){
+            return res.status(401).send({status:'error', msg:'Token verification failed', error: e})
+        }
+        res.status(500).send({status:'error', msg:'An error occured'})
+        
+    }
+})
+
+//endpoint to set task privacy
+router.post('/set_privacy', async(req, res) =>{
+    const {token, privacy_status} = req.body
+    if(!token || !privacy_status)
+        return res.status(400).send({status: 'error', msg:'All fields must be filled'});
+
+    try {
+        //verify token
+        const user = jwt.verify(token, process.env.JWT_SECRET)
+
+         //get task document
+         const task = await Task.findOne({task_id, user_id: user._id}, {privacy_status: 1}).lean()
+
+        task = await Task.findByIdAndUpdate({task_id, user_id: user._id}, {privacy_status: privacy_status})
+
+        return res.status(200).send({status:'ok', msg:'Privacy status updated'})
+    } catch (e) {
+        console.log(e)
+        if(e.name === 'JsonWebTokenError'){
+            return res.status(401).send({status:'error', msg:'Token verification failed', error: e})
+        }
+        res.status(500).send({status:'error', msg:'An error occured'}) 
+    }
+})
+
+//add task to project
+router.post('/add_to_project', async(req, res) =>{
+    const {token , task_id, project_id} = req.body
+    if(!task_id || !token || !project_id)
+        return res.status(400).send({status: 'error', msg:'All fields must be filled'});
+
+    try {
+        //verify token
+        const user = jwt.verify(token, process.env.JWT_SECRET)
+
+        //get project document
+        const project = await Project.findOne({project_id, user_id: user._id}, {tasks: 1}).lean()
+        if(!project)
+            return res.status(200).send({status: 'ok', msg: 'No project found'})
+
+        project = await Project.findOneAndUpdate({project_id, user_id: user._id}, {$push: {tasks: task_id}}, {new: true}).lean()
+
+        return res.status(200).send({status: 'ok', msg: 'Add to project successful'})
+        
+    } catch (e) {
+        console.log(e)
+        if(e.name === 'JsonWebTokenError'){
+            return res.status(401).send({status:'error', msg:'Token verification failed', error: e})
+        }
+        res.status(500).send({status:'error', msg:'An error occured'})  
+    }
+})
+
+//set priority
+router.post('/set_priority', async (req,res) =>{
+    const {token,  priority, task_id} =req.body
+    if(!token || !priority ||!task_id)
+        return res.status(400).send({status: 'error', msg:'All fields must be filled'});
+
+    try {
+        //verify token
+        const user = jwt.verify(token, process.env.JWT_SECRET)
+
+        const task = await Project.tasks.findone({task_id: task_id}, {priority: 1}).lean()
+        if(!task)
+            return res.status(200).send({status: 'ok', msg: 'No project found'})
+
+        task = await Project.tasks.findOneAndUpdate({task_id: task_id}, {priority: priority}).lean()
+
+        return res.status(200).send({status: 'ok', msg:'Priority Set successfully'})
+
+    } catch (e) {
+        console.log(e)
+        if(e.name === 'JsonWebTokenError'){
+            return res.status(401).send({status:'error', msg:'Token verification failed', error: e})
+        }
+        res.status(500).send({status:'error', msg:'An error occured'}) 
+    }
+})
+
+
+ 
+
+module.exports = router
